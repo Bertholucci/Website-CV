1da5aad3fa1e3d65bece73761001a0bcb1e88f8f
Matheus Bertholucci <matheusbertholucci12@gmail.com>
2021-07-07 22:52:14 -0300

    Created the "contact" layout. There is still some contents missings, have to do modifications later.

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#typed', {
+    strings: ["<blue> Habilidades </blue>", "<red> Competências </red>", "<green> Skills </green>"],
+    typeSpeed: 200,
+    startDelay: 30,
+    backSpeed: 300,
+    loop: true
+  });
